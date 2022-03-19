8e595d42e3d3a7c2d82001483c88a54fd504ad16
Francislainy Campos <francislainy.campos@hmhco.com>
2022-03-19 07:35:53 +0000

    Repositon table header

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -1,6 +1,4 @@
-const API_KEY = '3178e470764104af3454f4c7b542f2fb';
-// const API_BASE = 'https://api.themoviedb.org/3';
-const API_BASE = 'http://localhost:8081';
+const API_BASE = 'http://localhost:8080';
 
 const basicFetch = async (endpoint) => {
     const req = await fetch(`${API_BASE}${endpoint}`)
@@ -11,7 +9,6 @@ export default {
     getHomeList: async () => {
         return [
             {
-                slug: 'originals',
                 title: 'Originais do Netflix',
                 items: await basicFetch(`/api/mb/movie`)
             },
--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -1,25 +1,39 @@
 import React from 'react';
 import './MovieRow.css';
 
-export default ({title, items}) => {
-
+const MovieRow = ({title, items}) => {
     return (
         <div>
             <h2>{title}</h2>
             <h2>{items.movies.length}</h2>
             <div className="movieRow--listarea">
-                {items.movies.length > 0 && items.movies.map((item, key) => (
-                    <div>
-                        <span>{`${item.pinyin} `}</span>
-                        <span>{`${item.character} `}</span>
-                        <span>{`${item.setLocation.title} `}</span>
-                        <span>{`${item.actor.name} `}</span>
-                        <span>{`${item.meaning} `}</span>
-                        <span>{`${item.scene} `}</span>
-                        <span>{`${item.imageUrl} `}</span>
-                    </div>
-                ))}
+                <table className="table table-bordered table-dark">
+                    <thead>
+                    <th>Pinyin</th>
+                    <th>Character</th>
+                    <th>Set Location</th>
+                    <th>Actor</th>
+                    <th>Meaning</th>
+                    <th>Scene</th>
+                    <th>Image Url</th>
+                    </thead>
+                    {items.movies.length > 0 && items.movies.map((item, key) => (
+                        <tbody className="table-dark">
+                        <tr>
+                            <td>{`${item.pinyin}`}</td>
+                            <td>{`${item.character}`}</td>
+                            <td>{`${item.setLocation.title}`}</td>
+                            <td>{`${item.actor.name}`}</td>
+                            <td>{`${item.meaning}`}</td>
+                            <td>{`${item.scene}`}</td>
+                            <td>{`${item.imageUrl}`}</td>
+                        </tr>
+                        </tbody>
+                    ))}
+                </table>
             </div>
         </div>
     )
 }
+
+export default MovieRow;
