29283a7a64effaabddae07d5412038011801bc78
Francislainy Campos <francislainy.campos@hmhco.com>
2022-03-30 07:36:18 +0100

    Console.log data from form

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import {Button} from "react-bootstrap";
 const App = () => {
 
     const [movieList, setMovieList] = useState([])
+    const [data, setData] = useState(null)
+    const [show, setShow] = useState(false)
 
     const sampleList = [
         {
@@ -49,6 +51,24 @@ const App = () => {
         loadAll();
     }, []);
 
+    function getData(event) {
+        setData(event.target.value)
+        console.log(event.target.value)
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        const {character, pinyin, actor, location, room, scene} = e.target
+        console.log({
+            character: character.value,
+            pinyin: pinyin.value,
+            actor: actor.value,
+            location: location.value,
+            room: room.value,
+            scene: scene
+        })
+    }
+
     return (
         <div className="page">
             <nav className="navbar navbar-light navbar-expand-lg navigation-clean">
@@ -75,8 +95,43 @@ const App = () => {
 
             <div className="container">
                 <div className="button-container">
-                    <Button className="new-movie-button shadow-none">New Movie</Button>
+                    <Button className="new-movie-button shadow-none" onClick={() => setShow(true)}>New Movie</Button>
                 </div>
+                {
+                    // show? <input type={"text"} onChange={getData}/> : null
+                    show ? <section style={{paddingBottom: '16px'}}>
+                        <form method="post" onSubmit={handleSubmit}>
+                            <h2>Create new movie</h2>
+                            <div className="mb-2">
+                                <input className="form-control" type="text" name="character" placeholder="Character"
+                                       onChange={getData}/>
+                            </div>
+                            <div className="mb-2">
+                                <input className="form-control" type="text" name="pinyin" placeholder="Pinyin"
+                                       onChange={getData}/>
+                            </div>
+                            <div className="mb-2">
+                                <input className="form-control" type="text" name="actor" placeholder="Actor"
+                                       onChange={getData}/>
+                            </div>
+                            <div className="mb-2">
+                                <input className="form-control" type="text" name="location" placeholder="Location"
+                                       onChange={getData}/>
+                            </div>
+                            <div className="mb-2">
+                                <input className="form-control" type="text" name="room" placeholder="Room"
+                                       onChange={getData}/>
+                            </div>
+                            <div className="mb-2">
+                                <textarea className="form-control" name="scene" placeholder="Scene" rows="14"
+                                          onChange={getData}></textarea>
+                            </div>
+                            <div>
+                                <button className="btn btn-warning submit-button" type="submit">Send</button>
+                            </div>
+                        </form>
+                    </section> : null
+                }
                 <section className="lists">
                     {movieList.map((item, key) => (
                         <MovieRow key={key} title={item.title} items={item.items}/>
