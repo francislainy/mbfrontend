ef746260d1b4bda1a5d0c66588902def7d5ab6cf
Francislainy Campos <francislainy.campos@hmhco.com>
2022-04-01 00:29:35 +0100

    Layout improvements for create new movie form

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {Button, Dropdown, DropdownButton} from "react-bootstrap";
 const App = () => {
 
     const [showForm, setShowForm] = useState(false)
+    const [showNewMovieButton, setShowNewMovieButton] = useState(true)
     const [showSuccessAlert, setShowSuccessAlert] = useState(false)
     const [selectedActorId, selectActorId] = useState();
     const [selectedLocationId, selectLocationId] = useState();
@@ -146,6 +147,16 @@ const App = () => {
         }
     ]
 
+    const handleShowForm = () => {
+        setShowForm(true)
+        setShowNewMovieButton(false)
+    }
+
+    const handleHideForm = () => {
+        setShowForm(false)
+        setShowNewMovieButton(true)
+    }
+
     return (
         <div className="page">
             <nav className="navbar navbar-light navbar-expand-lg navigation-clean">
@@ -172,90 +183,100 @@ const App = () => {
             {showSuccessAlert ? getAlert() : null}
             <div className="container">
                 <div className="button-container">
-                    <Button className="new-movie-button shadow-none" onClick={() => setShowForm(true)}>New
+                    {showNewMovieButton && <Button className="new-movie-button shadow-none" onClick={handleShowForm}>New
                         Movie</Button>
+                    }
                 </div>
                 {
                     showForm ? <section style={{paddingBottom: '16px'}}>
                         <form method="post" onSubmit={handleSubmit}>
                             <div className="close-form-container">
                                 <Button className="new-movie-button shadow-none"
-                                        onClick={() => setShowForm(false)}>X</Button>
+                                        onClick={handleHideForm}>X</Button>
                             </div>
                             <h2 style={{color: "#f5b615"}}>Create new movie</h2>
-                            <div className="mb-2">
-                                <input className="form-control" type="text" name="character" placeholder="Character"/>
-                            </div>
-                            <div className="mb-2">
-                                <input className="form-control" type="text" name="pinyin" placeholder="Pinyin"/>
-                            </div>
-                            <div className="mb-2">
-                                <input className="form-control" type="text" name="meaning" placeholder="Meaning"/>
-                            </div>
-                            <div className="mb-2">
-                                <DropdownButton
-                                    title={
-                                        selectedActorId
-                                            ? actors.find((actor) => actor.id === selectedActorId).name
-                                            : "Select actor"
-                                    }
-                                    onSelect={(eventKey) => selectActorId(eventKey)}>
-                                    {actors.map((actor, index) => {
-                                        return (
-                                            <Dropdown.Item
-                                                key={index}
-                                                eventKey={actor.id}
-                                                active={actor.id === selectedActorId}>
-                                                {actor.name}
-                                            </Dropdown.Item>
-                                        );
-                                    })}
-                                </DropdownButton>
-                            </div>
-                            <div className="mb-2">
-                                <DropdownButton
-                                    title={
-                                        selectedLocationId
-                                            ? locations.find((location) => location.id === selectedLocationId).title
-                                            : "Select location"
-                                    }
-                                    onSelect={(eventKey) => selectLocationId(eventKey)}>
-                                    {locations.map((location, index) => {
-                                        return (
-                                            <Dropdown.Item
-                                                key={index}
-                                                eventKey={location.id}
-                                                active={location.id === selectedLocationId}>
-                                                {location.title}
-                                            </Dropdown.Item>
-                                        );
-                                    })}
-                                </DropdownButton>
-                            </div>
-                            <div className="mb-2">
-                                <DropdownButton
-                                    title={
-                                        selectedRoomId
-                                            ? rooms.find((room) => room.id === selectedRoomId).title
-                                            : "Select room"
-                                    }
-                                    onSelect={(eventKey) => selectRoomId(eventKey)}>
-                                    {rooms.map((room, index) => {
-                                        return (
-                                            <Dropdown.Item
-                                                key={index}
-                                                eventKey={room.id}
-                                                active={room.id === selectedRoomId}>
-                                                {room.title}
-                                            </Dropdown.Item>
-                                        );
-                                    })}
-                                </DropdownButton>
-                            </div>
-                            <div className="mb-2">
+                            <div className="a">
+                                <div className="row">
+                                    <div className="mb-2 col-2">
+                                        <input className="form-control" type="text" name="character"
+                                               placeholder="Character"/>
+                                    </div>
+                                    <div className="mb-2 col-2">
+                                        <input className="form-control" type="text" name="pinyin" placeholder="Pinyin"/>
+                                    </div>
+                                    <div className="mb-2 col-8">
+                                        <input className="form-control" type="text" name="meaning"
+                                               placeholder="Meaning"/>
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className="mb-2 col-4">
+                                        <DropdownButton variant="success"
+                                                        title={
+                                                            selectedActorId
+                                                                ? actors.find((actor) => actor.id === selectedActorId).name
+                                                                : "Select actor"
+                                                        }
+                                                        onSelect={(eventKey) => selectActorId(eventKey)}>
+                                            {actors.map((actor, index) => {
+                                                return (
+                                                    <Dropdown.Item
+                                                        key={index}
+                                                        eventKey={actor.id}
+                                                        active={actor.id === selectedActorId}>
+                                                        {actor.name}
+                                                    </Dropdown.Item>
+                                                );
+                                            })}
+                                        </DropdownButton>
+                                    </div>
+                                    <div className="mb-2 col-4">
+                                        <DropdownButton variant="success"
+                                                        title={
+                                                            selectedLocationId
+                                                                ? locations.find((location) => location.id === selectedLocationId).title
+                                                                : "Select location"
+                                                        }
+                                                        onSelect={(eventKey) => selectLocationId(eventKey)}>
+                                            {locations.map((location, index) => {
+                                                return (
+                                                    <Dropdown.Item
+                                                        key={index}
+                                                        eventKey={location.id}
+                                                        active={location.id === selectedLocationId}>
+                                                        {location.title}
+                                                    </Dropdown.Item>
+                                                );
+                                            })}
+                                        </DropdownButton>
+                                    </div>
+                                    <div className="mb-2 col-4">
+                                        <DropdownButton variant="success"
+                                                        title={
+                                                            selectedRoomId
+                                                                ? rooms.find((room) => room.id === selectedRoomId).title
+                                                                : "Select room"
+                                                        }
+                                                        onSelect={(eventKey) => selectRoomId(eventKey)}>
+                                            {rooms.map((room, index) => {
+                                                return (
+                                                    <Dropdown.Item
+                                                        key={index}
+                                                        eventKey={room.id}
+                                                        active={room.id === selectedRoomId}>
+                                                        {room.title}
+                                                    </Dropdown.Item>
+                                                );
+                                            })}
+                                        </DropdownButton>
+                                    </div>
+                                </div>
+                                <div className="mb-2 col-12">
                                 <textarea className="form-control" name="scene" placeholder="Scene"
                                           rows="14"></textarea>
+                                </div>
                             </div>
+
                             <div>
                                 <button className="btn btn-warning submit-button" type="submit"
                                         onSubmit={() => handleSubmit()}>Send
