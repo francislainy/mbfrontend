817a0af63456229b491e2863fbe14000e9133171
Francislainy Campos <francislainy.campos@hmhco.com>
2022-04-11 13:22:31 +0100

    Fix test for getLocation

--- src/pact/api.pact.spec.js
+++ src/pact/api.pact.spec.js
@@ -1,12 +1,24 @@
-import { Pact } from '@pact-foundation/pact';
-// import { API } from './api';
-import { Matchers } from '@pact-foundation/pact';
-import {getLocations} from "../api";
-const { eachLike, regex } = Matchers;
+import {Pact} from '@pact-foundation/pact';
+
+
+import {Matchers} from '@pact-foundation/pact';
+
+const {eachLike, regex} = Matchers;
+
+const axios = require("axios");
+axios.defaults.baseURL = "http://127.0.0.1:9999";
+const port = 9999;
+const axiosClient = axios.create({
+    baseURL: `http://127.0.0.1:${port}/api`,
+});
 
 const mockProvider = new Pact({
-    consumer: 'pactflow-example-consumer',
-    provider: process.env.PACT_PROVIDER ? process.env.PACT_PROVIDER : 'pactflow-example-provider',
+    port,
+    consumer: "pactflow-example-consumer",
+    provider: process.env.PACT_PROVIDER
+        ? process.env.PACT_PROVIDER
+        : "pactflow-example-provider",
+    cors: true,
 });
 
 describe('API Pact test', () => {
@@ -17,7 +29,7 @@ describe('API Pact test', () => {
     describe('retrieving locations', () => {
         test('location exists', async () => {
             // set up Pact interactions
-            const expectedLocation = { id: '10', title: 'a title' }
+            const expectedLocation = {id: '10', title: 'a title'}
 
             await mockProvider.addInteraction({
                 state: 'locations exist',
@@ -32,16 +44,21 @@ describe('API Pact test', () => {
                 willRespondWith: {
                     status: 200,
                     headers: {
-                        'Content-Type': regex({generate: 'application/json; charset=utf-8', matcher: 'application/json;?.*'}),
+                        'Content-Type': regex({
+                            generate: 'application/json; charset=utf-8',
+                            matcher: 'application/json;?.*'
+                        }),
                     },
                     body: eachLike(expectedLocation),
                 },
             });
 
-            // const api = new API(mockProvider.mockService.baseUrl);
-
-            // make request to Pact mock server
-            const locations = await getLocations(mockProvider.mockService.baseUrl)
+            const locations = await axiosClient.request({
+                method: "GET",
+                url: `/mb/location/`,
+                headers: { Accept: "application/json" },
+            });
+            console.log("## locations", locations);
 
             // // assert that we got the expected response
             // expect(products).toStrictEqual([new Product(expectedProduct)]);
